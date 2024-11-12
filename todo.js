8f27434048c150d77ba08f842994ae6906af2baf
missak-hub <missak225@gmail.com>
2024-11-12 14:50:36 +0500

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,15 @@
+let myArray=[1,'ALIZA',{text:'something'}]
+
+console.log(Array.isArray(myArray))
+
+console.log(myArray.length)
+
+//myArray.splice(0,2)
+//console.log(myArray);
+
+myArray.push('orange');
+
+console.log(myArrray[2]);
+
+let mixedBag=['book',5,true,['nested','array']]
+console.log(mixedBag);
