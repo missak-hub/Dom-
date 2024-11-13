973a2d017bc2ff5e82816bb417b51c5e9fb87bca
missak-hub <missak225@gmail.com>
2024-11-13 14:24:49 +0500

    Add files via upload

---
+++ todolist1.js
@@ -0,0 +1,7 @@
+const todoList=[];
+function addToDo(){
+    let inputText=document.querySelector('.js-name-input')
+    todoList.push(inputText.value)
+    console.log(todoList)
+    inputText.value=''
+}
